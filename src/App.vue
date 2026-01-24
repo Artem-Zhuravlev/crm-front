293b0cbb4e65b2822967509c7e9d09298234f2e8
Zhuravlev Artem <mstrjesm@gmail.com>
2026-01-24 14:04:10 +0200

    init proj

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import BaseHeader from './layouts/BaseHeader.vue'
+</script>
+
+<template>
+  <base-header />
+  <router-view />
+</template>
+
+<style scoped></style>
---
+++ src/layouts/BaseHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="header">
+    <div class="header-logo">Jelly CRM</div>
+  </header>
+</template>
+
+<script setup lang="ts">
+const handleCommand = (command: string) => {
+  if (command === 'logout') {
+    console.log('Logout')
+  } else if (command === 'settings') {
+    console.log('Settings')
+  }
+}
+</script>
